c11e0e6e9662831e4433663acf969234d1f0194e
olivier-tan-ics4u1-02-25 <olivier.tan@student.tdsb.on.ca>
2025-04-03 23:39:28 +0000

    tests passed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class PracticeProblem {
     //q1
-    public static int compareSearch(int [] array, int number){
+    public static int[] compareSearch(int [] array, int number){
         int loop1 = 0;
         int loop2 = 0; //loops for counting
         Arrays.sort(array);
@@ -10,7 +10,7 @@ public class PracticeProblem {
         for (int i = 0; i < array.length; i++){
             loop1++; //counting loops
             if (array[i] == number){
-            return i; //break
+                break;
             }
         }
         //binary searching
@@ -35,51 +35,54 @@ public class PracticeProblem {
                 loop2++;
             }
         }
-            int loops = loop1 + loop2;
-            return loops;
+            int [] numarr = {loop1, loop2};
+            return numarr;
         }   
- }
 
 //q2
-    public static int compareStringSearch(String [] array, String word){
-        int loop1 = 0;
-        int loop2 = 0;
-        Arrays.sort(array);
+public static int[]compareStringSearch(String[] array, String text){
+    int loop1 = 0;
+    int loop2 = 0;
+    Arrays.sort(array);
 
-        //casing
-        for (int i = 0; i < array.length; i++){
-            array[i].toLowerCase();
-        }
-        
-        //sequential
-        for (int i = 0; i < array.length; i++){
-            loop1++;
-            if (array[i] == word){
-                return i;
-            }
-        }
 
-        //binary
-        int start = 0;
-        int end = 0;
-        int middle = (start + end)/2;
-
-        while (start <= end){
-            loop2++;
-            int comparison = array[middle].compareTo(word);
-            if (comparison < 0){
-                start = middle + 1;
-                middle = (start + end)/2;
-            }
-            else if (comparison > 0){
-                end = middle - 1;
-                middle = (start + end)/2;
-            }
-            else {
-                start = end + 1;
-            }
-        }
-    
-        int stringloops = loop1 + loop2;
-        return stringloops;
+    for (int i = 0;i < array.length;i++){
+        array[i].toLowerCase();
     }
+
+
+    for (int i = 0;i < array.length;i++){
+        loop1++;
+    if (array[i].equals(text)){
+        break;
+    }
+}
+
+
+    int start = 0;
+    int end = array.length - 1;
+    int mid = (start + end)/2;
+
+
+    while (start <= end) {
+    loop2++;
+        int comparison = array[mid].compareTo(text);
+        if (comparison < 0) {
+            start = mid + 1;
+            mid = (end + start) / 2;
+        }
+        else if (comparison > 0) {
+            end = mid - 1;
+            mid = (end + start) / 2;
+        }
+        else {
+            start = end + 1;
+        }
+    }
+
+
+int [] stringArr = {loop1, loop2};
+return stringArr;
+}
+}
+
